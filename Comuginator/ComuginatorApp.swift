bcf93c2012a543111725c6e94bed803b8e3f4bb5
Stepan Smagin <smagines@gmail.com>
2026-05-28 14:17:56 +0200

    Initial Commit

---
+++ Comuginator/ComuginatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComuginatorApp.swift
+//  Comuginator
+//
+//  Created by Stepan Smagin on 28/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct ComuginatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
